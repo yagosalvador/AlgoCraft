96428bdba5408a5708544fb9f1415988ed78ea14
BarbichMateo <mateobarbich52@gmail.com>
2019-06-04 23:21:23 -0300

    Agrego chequeo de herramienta inicial

--- test/Jugador.java
+++ test/Jugador.java
@@ -8,8 +8,12 @@ public class TestJugador{
 		Jugador jugador = new Jugador();
 		int cantidadDeHerramientasEnInventario = jugador.cantidadDeHerramientas();
 		assertEquals(cantidadDeHerramientasEnInventario,1);
-		Lista listaHerramientas = jugador.obetenerListaHerramientas();
+		Lista listaHerramientas = jugador.obtenerListaHerramientas();
 
 		//Como chequeamos que la herramienta que obtenemos es Hacha de madera? Hmm
+		//Propuesta, suponiendo que obtenerListaHerramientas va llenando listaHerramientas como un vector, no se como pensaron implementarla:
+		HachaDeMadera hacha = new HachaDeMadera();
+		assertEquals(hacha,listaHerramientas[1]);
+		//pregunta, habria que chequear las demas cosas que se deberian inicializar con el jugador, como el inventario por ejemplo?
 	}
 }
