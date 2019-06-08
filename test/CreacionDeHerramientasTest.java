709acd203271dd12afc5100f58c1da33885a6650
Wagner Marcos <marcoswagneer.18@gmail.com>
2019-06-08 19:37:25 -0300

    Corrijo errores de sintaxis

--- test/CreacionDeHerramientasTest.java
+++ test/CreacionDeHerramientasTest.java
@@ -3,99 +3,99 @@ import org.junit.Test;
 
 public class CreacionDeHerramientasTest{
     @Test
-    public void CreacionDeHachaDeMaderaTest{
-        MesaDeTrabajo mesa = MesaDeTrabajo new;
-        Material madera = Madera new;
+    public void CreacionDeHachaDeMaderaTest(){
+        MesaDeTrabajo mesa = new MesaDeTrabajo();
+        Material madera = new Madera();
         mesa.ubicarMaterial(1, madera);
         mesa.ubicarMaterial(2, madera);
         mesa.ubicarMaterial(4, madera);
         mesa.ubicarMaterial(5, madera);
         mesa.ubicarMaterial(8, madera);
         Herramienta hachaConstruida = mesa.construirHerramienta();
-        Herramienta unHacha = HachaDeMadera new;
+        Herramienta unHacha = new HachaDeMadera();
 
         assertEquals(unHacha.getClass(), hachaConstruida.getClass());
     }
     @Test
-    public void CreacionDeHachaDePiedraTest{
-        MesaDeTrabajo mesa = MesaDeTrabajo new;
-        Material madera = Madera new;
-        Material piedra = Piedra new;
+    public void CreacionDeHachaDePiedraTest(){
+        MesaDeTrabajo mesa = new MesaDeTrabajo();
+        Material madera = new Madera();
+        Material piedra = new Piedra();
         mesa.ubicarMaterial(1, piedra);
         mesa.ubicarMaterial(2, piedra);
         mesa.ubicarMaterial(4, piedra);
         mesa.ubicarMaterial(5, madera);
         mesa.ubicarMaterial(8, madera);
         Herramienta hachaConstruida = mesa.construirHerramienta();
-        Herramienta unHacha = HachaDeMadera new;
+        Herramienta unHacha = new HachaDeMadera();
 
         assertEquals(unHacha.getClass(), hachaConstruida.getClass());
     }
     @Test
-    public void CreacionDeHachaDeMetalTest{
-        MesaDeTrabajo mesa = MesaDeTrabajo new;
-        Material madera = Madera new;
-        Material metal = Metal new;
+    public void CreacionDeHachaDeMetalTest(){
+        MesaDeTrabajo mesa = new MesaDeTrabajo();
+        Material madera = new Madera();
+        Material metal = new Metal();
         mesa.ubicarMaterial(1, metal);
         mesa.ubicarMaterial(2, metal);
         mesa.ubicarMaterial(4, metal);
         mesa.ubicarMaterial(5, madera);
         mesa.ubicarMaterial(8, madera);
         Herramienta hachaConstruida = mesa.construirHerramienta();
-        Herramienta unHacha = HachaDeMadera new;
+        Herramienta unHacha = new HachaDeMadera();
 
         assertEquals(unHacha.getClass(), hachaConstruida.getClass());
     }
     @Test
-    public void CreacionDePicoDeMaderaTest{
-        MesaDeTrabajo mesa = MesaDeTrabajo new;
-        Material madera = Madera new;
+    public void CreacionDePicoDeMaderaTest(){
+        MesaDeTrabajo mesa = new MesaDeTrabajo();
+        Material madera = new Madera();
         mesa.ubicarMaterial(1, madera);
         mesa.ubicarMaterial(2, madera);
         mesa.ubicarMaterial(3, madera);
         mesa.ubicarMaterial(5, madera);
         mesa.ubicarMaterial(8, madera);
         Herramienta hachaConstruido = mesa.construirHerramienta();
-        Herramienta unPico = PicoDeMadera new;
+        Herramienta unPico = new PicoDeMadera();
 
         assertEquals(unHacha.getClass(), hachaConstruida.getClass());
     }
     @Test
-    public void CreacionDePicoDePiedraTest{
-        MesaDeTrabajo mesa = MesaDeTrabajo new;
-        Material madera = Madera new;
-        Material piedra = Piedra new;
+    public void CreacionDePicoDePiedraTest(){
+        MesaDeTrabajo mesa = new MesaDeTrabajo();
+        Material madera = new Madera();
+        Material piedra = new Piedra();
         mesa.ubicarMaterial(1, piedra);
         mesa.ubicarMaterial(2, piedra);
         mesa.ubicarMaterial(3, piedra);
         mesa.ubicarMaterial(5, madera);
         mesa.ubicarMaterial(8, madera);
         Herramienta picoConstruido = mesa.construirHerramienta();
-        Herramienta unPico = PicoDeMadera new;
+        Herramienta unPico = new PicoDeMadera();
 
         bool seConstruyoHacha = unPico.equals(hachaConstruida);
         assertEquals(true , seConstruyoPico);
     }
     @Test
-    public void CreacionDePicoDeMetalTest{
-        MesaDeTrabajo mesa = MesaDeTrabajo new;
-        Material madera = Madera new;
-        Material metal = Metal new;
+    public void CreacionDePicoDeMetalTest(){
+        MesaDeTrabajo mesa = new MesaDeTrabajo();
+        Material madera = new Madera();
+        Material metal = new Metal();
         mesa.ubicarMaterial(1, metal);
         mesa.ubicarMaterial(2, metal);
         mesa.ubicarMaterial(3, metal);
         mesa.ubicarMaterial(5, madera);
         mesa.ubicarMaterial(8, madera);
         Herramienta picoConstruido = mesa.construirHerramienta();
-        Herramienta unPico = PicoDeMadera new;
+        Herramienta unPico = new PicoDeMadera();
 
         assertEquals(unHacha.getClass(), hachaConstruida.getClass());;
     }
     @Test
-    public void CreacionDePicoFinoTest{
-        MesaDeTrabajo mesa = MesaDeTrabajo new;
-        Material madera = Madera new;
-        Material metal = Metal new;
+    public void CreacionDePicoFinoTest(){
+        MesaDeTrabajo mesa = new MesaDeTrabajo();
+        Material madera = new Madera();
+        Material metal = new Metal();
         mesa.ubicarMaterial(1, metal);
         mesa.ubicarMaterial(2, metal);
         mesa.ubicarMaterial(3, metal);
@@ -103,7 +103,7 @@ public class CreacionDeHerramientasTest{
         mesa.ubicarMaterial(5, madera);
         mesa.ubicarMaterial(8, madera);
         Herramienta picoConstruido = mesa.construirHerramienta();
-        Herramienta unPico = PicoDeMadera new;
+        Herramienta unPico = new PicoDeMadera();
 
         assertEquals(unHacha.getClass(), hachaConstruida.getClass());
     }
