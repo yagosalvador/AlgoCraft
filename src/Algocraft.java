a49867a663fa1ae1ea81621c4d39cbb097592ba3
gabbo3 <42916465+gabbo3@users.noreply.github.com>
2019-06-12 16:16:28 -0300

    Add files via upload

---
+++ src/Algocraft.java
@@ -0,0 +1,11 @@
+public class Algocraft{
+	private Mapa mapa;
+
+	public Mapa mapa(){
+		return mapa;
+	}
+	public static void main(String[] args) {
+		System.out.println("Iniciando Algocraft - Bienvenido");
+	}
+
+}
