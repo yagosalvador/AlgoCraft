a3aafcfcf28623ca8f645595d00ebf55d701b77e
gabbo3 <42916465+gabbo3@users.noreply.github.com>
2019-06-13 11:19:59 -0300

    Add files via upload

---
+++ test/MovilidadDelJugadorTest.java
@@ -0,0 +1,27 @@
+/*
+import static org.junit.Assert.*;
+import org.junit.Test;
+
+public class MovilidadDelJuegadorTest{
+	public void jugadorSeMueveAUnaPosicionVacia() {
+		Mapa mapa = new Mapa();
+		Jugador jugador = new Jugador();
+		mapa.celda(0,0).ocupar(jugador);
+		// El jugador se desplaza a la derecha y puede moverse
+		
+	}
+	public void jugadorNoSePuedeMoverAPosicionOcupada() {
+		Mapa mapa = new Mapa();
+		Jugador jugador = new Jugador();
+		Madera maderaTest = new Madera();
+		mapa.celda(0,0).ocupar(jugador);
+		mapa.celda(0,1).ocupar(maderaTest);
+		// El jugador se desplaza a la derecha y no puede moverse
+	}
+	public void jugadorAvanza(){
+		Algocraft juego = new Algocraft();
+		// algocraft se inicia con el jugador en (0,0)
+				
+	}
+}
+*/
