be6476b7171b34db8cce12aa2993468f82175018
Wagner Marcos <marcoswagneer.18@gmail.com>
2019-06-15 03:39:20 -0300

    Subo receta con un error. Ver si pueden resolverlo.

--- src/Receta.java
+++ src/Receta.java
@@ -1,34 +1,37 @@
 //Posible implementacion de receta
+//import static java.lang.System.*;
+import java.util.HashMap;
+import java.util.Map;
 
 public class Receta{
 
-    private Material [ ] receta = new Material [9];
+    private Map<Integer, Material> receta = new HashMap<>(9);
 
-    public Receta(Material material1, Material material2, Material material3,
-                  Material material4, Material material5, Material material6,
-                  Material material7, Material material8, Material material9){
-        this.receta[0] = material1;
-        this.receta[1] = material2;
-        this.receta[2] = material3;
-        this.receta[3] = material4;
-        this.receta[4] = material5;
-        this.receta[5] = material6;
-        this.receta[6] = material7;
-        this.receta[7] = material8;
-        this.receta[8] = material9;
+    public Receta(){
+        Material noObject = new NoObject();
+        receta.put(0, noObject);
+        receta.put(1, noObject);
+        receta.put(2, noObject);
+        receta.put(3, noObject);
+        receta.put(4, noObject);
+        receta.put(5, noObject);
+        receta.put(6, noObject);
+        receta.put(7, noObject);
+        receta.put(8, noObject);
     }
 
     public boolean equals(Receta unaReceta){
         boolean equals = true;
 
         for(int i = 0; i < 9 ; i ++) {
-            if (this.receta[i].esMismoMaterialQue(unaReceta.receta[i]) != true)
+            Material material = unaReceta.receta.getValue(i);
+            if (this.receta.get(i).esMismoMaterialQue(material) = true)
                 equals = false;
         }
         return equals;
     }
 
     public void ubicarMaterial(int posicion, Material material){
-        this.receta[posicion] = material;
+        receta.replace(posicion, material);
     }
 }
